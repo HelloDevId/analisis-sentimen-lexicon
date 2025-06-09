0c6448633a825477483a91e0d98d9a001896e8d5
ilhamibnu <e41210178@student.polije.ac.id>
2025-06-09 08:17:59 +0700

    .

--- resources/views/admin2/layout/main.blade.php
+++ resources/views/admin2/layout/main.blade.php
@@ -65,12 +65,12 @@
                     document.documentElement.setAttribute("data-color-theme", e);
                 }
             </script>
-            <button
+            {{-- <button
                 class="btn btn-primary p-3 rounded-circle d-flex align-items-center justify-content-center customizer-btn"
                 type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
                 aria-controls="offcanvasExample">
                 <i class="icon ti ti-settings fs-7"></i>
-            </button>
+            </button> --}}
 
             <div class="offcanvas customizer offcanvas-end" tabindex="-1" id="offcanvasExample"
                 aria-labelledby="offcanvasExampleLabel">
